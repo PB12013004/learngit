d87bd8b2943bc2bcf2177212c0239df7ed873f65
ylang <ly0405@mail.ustc.edu.cn>
2016-01-02 21:19:11 +0800

    modify readme.cc and generate a.out

--- readme.cc
+++ readme.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 int main(){
-  std::cout << input two integers << endl;
+  std::cout << "input two integers" << std::endl;
+  int v1 = 0, v2 = 0;
   std::cin >> v1 >> v2;
   std::cout << "the sum is " << v1 + v2 << std::endl;
   return 0;
