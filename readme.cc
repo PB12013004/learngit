05a384bbbc1d5e3f20183dd4bba7c2ad1ddf1f28
ylang <ly0405@mail.ustc.edu.cn>
2016-01-02 17:31:27 +0800

    add modified

--- readme.cc
+++ readme.cc
@@ -1,5 +1,5 @@
 #include <iostream>
 int main(){
-  std::cout << "readme" << std::endl;
+  std::cout << "readme modified" << std::endl;
   return 0;
 }
--- readme2.c
+++ readme2.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 void main(){
-  printf("readme2");
+  printf("readme2 modified");
 }
