ecbb1246e0683182d9e2d23a1e5c87346f8aac02
ylang <ly0405@mail.ustc.edu.cn>
2016-01-02 21:11:36 +0800

    back to step2

--- readme.cc
+++ readme.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 int main(){
-  std::cout << "readme modified" << std::endl;
+  std::cout << input two integers << endl;
+  std::cin >> v1 >> v2;
+  std::cout << "the sum is " << v1 + v2 << std::endl;
   return 0;
 }
